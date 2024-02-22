b3886fc0e7ee19f32a33ba3d207e316efccbacfe
depriandiy <depriandianton22@gmail.com>
2024-02-22 12:04:37 +0700

    getx - tipe data

--- lib/controllers/home_controller.dart
+++ lib/controllers/home_controller.dart
@@ -1,17 +1,35 @@
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
-  // RxInt dataObs = 0.obs; // reactive state management (Rx) -> using Obx()
-  int dataObs = 0; // simple state management -> using GetBuilder()
+  RxInt dataInt = 0.obs;
+  RxString dataString = "data".obs;
+  RxBool dataBool = true.obs;
+  RxList<int> dataList = [1, 2, 3].obs;
+  RxMap<String, dynamic> dataMap = <String, dynamic>{
+    "id": 1,
+    "name": "Depri",
+    "age": 22,
+  }.obs;
 
-  void addData() {
-    dataObs++;
-    if (dataObs % 5 == 0) {
-      update();
-    }
+  void changeName() {
+    dataMap["name"] = "Rian";
+    dataMap["age"] = "23";
   }
 
-  void refreshData() {
-    update();
+  int nextNum = 4;
+  void addNumber() {
+    dataList.add(nextNum);
+    nextNum++;
   }
+
+  void switchBool() {
+    // dataBool.value != dataBool.value;
+    dataBool.toggle(); // cara simple
+  }
+
+  void updateString() => dataString.value = "data (updated)";
+  void resetString() => dataString.value = "data";
+
+  void incrementInt() => dataInt++;
+  void decrementInt() => dataInt--;
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,45 +10,154 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text("Belajar GetX"),
+        title: const Text(
+          "BELAJAR TIPE DATA RX",
+          style: TextStyle(color: Colors.white),
+        ),
         backgroundColor: Colors.blue,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // realtime changing.
-            // Obx(
-            //   () => Text(
-            //     "${homeC.dataObs}",
-            //     style: TextStyle(fontSize: 50),
-            //   ),
-            // ),
-
-            // simple SM, make a trigger to change state.
-            GetBuilder<HomeController>(
-              builder: (controller) => Text(
-                "${controller.dataObs}",
-                style: TextStyle(fontSize: 50),
+      body: ListView(
+        padding: EdgeInsets.all(30),
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Obx(
+                () => Text(
+                  "${homeC.dataInt}",
+                  style: TextStyle(fontSize: 30),
+                ),
               ),
-            ),
-            SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () {
-                homeC.addData();
-              },
-              child: Text("ADD DATA"),
-            ),
-            SizedBox(height: 10),
-            // button for trigger state changed.
-            ElevatedButton(
-              onPressed: () {
-                homeC.refreshData();
-              },
-              child: Text("REFRESH"),
-            ),
-          ],
-        ),
+              Row(
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      homeC.decrementInt();
+                    },
+                    child: const Text(
+                      "-",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  ElevatedButton(
+                    onPressed: () {
+                      homeC.incrementInt();
+                    },
+                    child: const Text(
+                      "+",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          SizedBox(height: 25),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Obx(
+                () => Text(
+                  "${homeC.dataString}",
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+              Row(
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      homeC.updateString();
+                    },
+                    child: const Text(
+                      "Update",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  ElevatedButton(
+                    onPressed: () {
+                      homeC.resetString();
+                    },
+                    child: const Text(
+                      "Reset",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          SizedBox(height: 25),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Obx(
+                () => Text(
+                  "${homeC.dataBool}",
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  homeC.switchBool();
+                },
+                child: const Text(
+                  "Switch",
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 25),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Obx(
+                () => Text(
+                  "${homeC.dataList}",
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  homeC.addNumber();
+                },
+                child: const Text(
+                  "Add Number",
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+            ],
+          ),
+          Divider(thickness: 2),
+          Row(
+            children: [
+              Expanded(
+                child: Obx(
+                  () => ListTile(
+                    leading: CircleAvatar(
+                      child: Text(
+                        "${homeC.dataMap['id']}",
+                      ),
+                    ),
+                    title: Text("${homeC.dataMap['name']}"),
+                    subtitle: Text("${homeC.dataMap['age']} tahun"),
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  homeC.changeName();
+                },
+                child: const Text(
+                  "Change",
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
