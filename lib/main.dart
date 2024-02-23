6f26e9848dfe00844a71b1a1ed01a50f53257e42
depriandiy <depriandianton22@gmail.com>
2024-02-23 15:03:40 +0700

    init get cli

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:gym_presence/bindings/home_binding.dart';
-import 'package:gym_presence/controllers/home_controller.dart';
-import 'package:gym_presence/screens/profile_screen.dart';
-import 'package:gym_presence/screens/splash_screen.dart';
-import 'firebase_options.dart';
 import 'package:get/get.dart';
-import 'package:gym_presence/screens/home_screen.dart';
+import 'app/routes/app_pages.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return GetMaterialApp(
+void main() {
+  runApp(
+    GetMaterialApp(
+      title: "Application",
       debugShowCheckedModeBanner: false,
-      title: 'Gym Presence',
-      home: HomeScreen(),
-      getPages: [
-        GetPage(name: "/intro", page: () => SplashScreen()),
-        GetPage(name: "/profile", page: () => ProfileScreen()),
-        GetPage(
-          name: "/home",
-          page: () => HomeScreen(),
-          // binding: BindingsBuilder.put(() => HomeController()),
-          binding: HomeBinding(), // call a class binding
-        ),
-      ],
-    );
-  }
+      initialRoute: AppPages.INITIAL,
+      getPages: AppPages.routes,
+    ),
+  );
 }
